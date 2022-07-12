afac90b04c1ffcf89e9fa5f2fd7e097275cc0612
MayaVydraYIR191 <vydra_mo_19@mf.grsu.by>
2022-07-12 13:21:07 +0300

    New version

---
+++ Assets/scripts/missioncomplete.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class missioncomplete : MonoBehaviour
+{
+    private int count; 
+    public GameObject missionCompleted;
+
+    public static bool isDelivered = false;
+    public static bool completed = false;
+
+    [SerializeField] Text countText;
+
+    void Awake()
+    {
+        isDelivered = false;
+        count = 0;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag != "Player" && other.gameObject.tag == "KeyItem")
+        {
+            MissionProgress.stores++;
+            count++;
+            isDelivered = true;
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+            CheckQuest();
+        }
+    }
+
+    void CheckQuest()
+    {
+        if (count == 3)
+        {
+            completed = true;
+            missionCompleted.SetActive(true);
+        }
+    }
+}
